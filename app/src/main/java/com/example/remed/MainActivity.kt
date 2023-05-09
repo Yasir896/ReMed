d4f197b6616da7a9592293b0a2625bf2556c12b6
Yasir Ghafar <yasir896@gmail.com>
2023-05-09 23:01:50 +0500

    Code Refactor in MainActivity

--- app/src/main/java/com/example/remed/MainActivity.kt
+++ app/src/main/java/com/example/remed/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.remed
 
+import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -12,13 +15,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import coil.annotation.ExperimentalCoilApi
 import com.example.remed.features.domain.alarm.AlarmScheduler
 import com.example.remed.features.domain.alarm.AlarmSchedulerImpl
 import com.example.remed.features.domain.model.Alarm
+import com.example.remed.features.presentation.components.CustomDialog
 import com.example.remed.navigation.AppNavigation
 import com.example.remed.ui.theme.ReMedTheme
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.LocalDateTime
 
@@ -35,7 +43,7 @@ class MainActivity : ComponentActivity() {
         var alarmTime: Alarm?
 
         setContent {
-            ReMedTheme {
+            /*ReMedTheme {
 
                 var secondText by remember { mutableStateOf("") }
                 var messageText by remember { mutableStateOf("") }
@@ -94,43 +102,45 @@ class MainActivity : ComponentActivity() {
 
                 }
 
-            }
+            }*/
+
+            installSplashScreen()
+          .apply {
+          setOnExitAnimationListener{ sp ->
+              sp.iconView.animate().rotation(180F).duration = 3000L
+              val slideUp = ObjectAnimator.ofFloat(
+                  sp.iconView,
+                  View.TRANSLATION_Y,
+                  0f,
+                  -sp.iconView.height.toFloat()
+              )
+              slideUp.interpolator = AnticipateInterpolator()
+              slideUp.duration = 3000L
+              // Call SplashScreenView.remove at the end of your custom animation.
+              slideUp.doOnEnd {
+                  sp.remove()
+              }
+              // Run your animation.
+              slideUp.start()
+      }
+  }
+      WindowCompat.setDecorFitsSystemWindows(window, true)
+      setContent {
+
+          val displayDialog = remember {
+              mutableStateOf(false)
+          }
+
+          val navController = rememberAnimatedNavController()
+          ThemeHandler(navController)
+
+          if (displayDialog.value)
+              CustomDialog(openCustomDialog = displayDialog){
+                  finish()
+              }
+      }
         }
-        /*installSplashScreen()
-            .apply {
-            setOnExitAnimationListener{ sp ->
-                sp.iconView.animate().rotation(180F).duration = 3000L
-                val slideUp = ObjectAnimator.ofFloat(
-                    sp.iconView,
-                    View.TRANSLATION_Y,
-                    0f,
-                    -sp.iconView.height.toFloat()
-                )
-                slideUp.interpolator = AnticipateInterpolator()
-                slideUp.duration = 3000L
-                // Call SplashScreenView.remove at the end of your custom animation.
-                slideUp.doOnEnd {
-                    sp.remove()
-                }
-                // Run your animation.
-                slideUp.start()
-        }
-    }
-        WindowCompat.setDecorFitsSystemWindows(window, true)
-        setContent {
 
-            val displayDialog = remember {
-                mutableStateOf(false)
-            }
-
-            val navController = rememberAnimatedNavController()
-            ThemeHandler(navController)
-
-            if (displayDialog.value)
-                CustomDialog(openCustomDialog = displayDialog){
-                    finish()
-                }
-        }*/
 }
 
 @ExperimentalAnimationApi
