1be0d2c6e44e9ed02f0abf2e28a6ef13b34fd47b
Yasir Ghafar <yasir896@gmail.com>
2023-05-11 22:30:51 +0500

    Alarm Test Screen introduced.

--- app/src/main/java/com/example/remed/MainActivity.kt
+++ app/src/main/java/com/example/remed/MainActivity.kt
@@ -23,6 +23,7 @@ import coil.annotation.ExperimentalCoilApi
 import com.example.remed.features.domain.alarm.AlarmScheduler
 import com.example.remed.features.domain.alarm.AlarmSchedulerImpl
 import com.example.remed.features.domain.model.Alarm
+import com.example.remed.features.presentation.AlarmTestScreen
 import com.example.remed.features.presentation.components.CustomDialog
 import com.example.remed.navigation.AppNavigation
 import com.example.remed.ui.theme.ReMedTheme
@@ -39,124 +40,61 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val alarmScheduler : AlarmScheduler = AlarmSchedulerImpl(this)
-        var alarmTime: Alarm?
-
         setContent {
-            /*ReMedTheme {
-
-                var secondText by remember { mutableStateOf("") }
-                var messageText by remember { mutableStateOf("") }
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 16.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-
-                    OutlinedTextField(value = secondText, onValueChange = {
-                        secondText = it
-                    },
-                        label = {
-                            Text(text = "Delay Second")
+            installSplashScreen()
+                .apply {
+                    setOnExitAnimationListener { sp ->
+                        sp.iconView.animate().rotation(180F).duration = 3000L
+                        val slideUp = ObjectAnimator.ofFloat(
+                            sp.iconView,
+                            View.TRANSLATION_Y,
+                            0f,
+                            -sp.iconView.height.toFloat()
+                        )
+                        slideUp.interpolator = AnticipateInterpolator()
+                        slideUp.duration = 3000L
+                        // Call SplashScreenView.remove at the end of your custom animation.
+                        slideUp.doOnEnd {
+                            sp.remove()
                         }
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    OutlinedTextField(value = messageText, onValueChange = {
-                        messageText = it
-                    },
-                        label = {
-                            Text(text = "Message")
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Button(onClick = {
-                            alarmTime = Alarm(
-                                alarmTime = LocalDateTime.now().plusSeconds(
-                                    secondText?.toLong()
-                                ),
-                                message = messageText
-                            )
-                            alarmTime?.let { alarmScheduler::schedule }
-                            secondText = ""
-                            messageText = ""
-
-                        }) {
-                            Text(text = "Schedule")
-                        }
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Button(onClick = {
-                            alarmScheduler::cancel
-                        }) {
-                            Text(text = "Cancel")
-                        }
-                        Spacer(modifier = Modifier.width(8.dp))
+                        // Run your animation.
+                        slideUp.start()
                     }
+                }
+            WindowCompat.setDecorFitsSystemWindows(window, true)
+            setContent {
 
+                val displayDialog = remember {
+                    mutableStateOf(false)
                 }
 
-            }*/
+                val navController = rememberAnimatedNavController()
+                ThemeHandler(navController)
 
-            installSplashScreen()
-          .apply {
-          setOnExitAnimationListener{ sp ->
-              sp.iconView.animate().rotation(180F).duration = 3000L
-              val slideUp = ObjectAnimator.ofFloat(
-                  sp.iconView,
-                  View.TRANSLATION_Y,
-                  0f,
-                  -sp.iconView.height.toFloat()
-              )
-              slideUp.interpolator = AnticipateInterpolator()
-              slideUp.duration = 3000L
-              // Call SplashScreenView.remove at the end of your custom animation.
-              slideUp.doOnEnd {
-                  sp.remove()
-              }
-              // Run your animation.
-              slideUp.start()
-      }
-  }
-      WindowCompat.setDecorFitsSystemWindows(window, true)
-      setContent {
-
-          val displayDialog = remember {
-              mutableStateOf(false)
-          }
-
-          val navController = rememberAnimatedNavController()
-          ThemeHandler(navController)
-
-          if (displayDialog.value)
-              CustomDialog(openCustomDialog = displayDialog){
-                  finish()
-              }
-      }
+                if (displayDialog.value)
+                    CustomDialog(openCustomDialog = displayDialog) {
+                        finish()
+                    }
+            }
         }
 
-}
+    }
 
-@ExperimentalAnimationApi
-@ExperimentalCoilApi
-@Composable
-fun ThemeHandler(navController: NavHostController) {
-    ReMedTheme {
-        Scaffold {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colors.background
-            ) {
-                AppNavigation(navController = navController)
+    @ExperimentalAnimationApi
+    @ExperimentalCoilApi
+    @Composable
+    fun ThemeHandler(navController: NavHostController) {
+        ReMedTheme {
+            Scaffold {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    //AppNavigation(navController = navController)
+                    AlarmTestScreen()
+                }
             }
         }
     }
-}
 
 }
---
+++ app/src/main/java/com/example/remed/features/presentation/AlarmTestScreen.kt
@@ -0,0 +1,82 @@
+package com.example.remed.features.presentation
+
+import android.os.Build
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.remed.features.domain.alarm.AlarmScheduler
+import com.example.remed.features.domain.alarm.AlarmSchedulerImpl
+import com.example.remed.features.domain.model.Alarm
+import java.time.LocalDateTime
+
+@Composable
+fun AlarmTestScreen() {
+
+    val alarmScheduler : AlarmScheduler = AlarmSchedulerImpl(LocalContext.current)
+    var alarmTime: Alarm? = null
+
+    var secondText by remember { mutableStateOf("") }
+    var messageText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        OutlinedTextField(value = secondText, onValueChange = {
+            secondText = it
+        },
+            label = {
+                Text(text = "Delay Second")
+            }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(value = messageText, onValueChange = {
+            messageText = it
+        },
+            label = {
+                Text(text = "Message")
+            }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    alarmTime = Alarm(
+                        alarmTime = LocalDateTime.now().plusSeconds(
+                            secondText?.toLong()
+                        ),
+                        message = messageText
+                    )
+                }
+                alarmTime?.let { alarmScheduler::schedule }
+                secondText = ""
+                messageText = ""
+
+            }) {
+                Text(text = "Schedule")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                alarmScheduler::cancel
+            }) {
+                Text(text = "Cancel")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+
+    }
+}
